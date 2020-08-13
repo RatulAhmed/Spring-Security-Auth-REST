26f060bba0d2782e0cf555e7d18e6d4ae9c3997d
ratulahmed <ratulahmed1@gmail.com>
2020-08-12 20:20:00 -0400

    Simple authentication for REST endpoints

---
+++ src/test/java/com/example/userlogic/UserlogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userlogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserlogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
